0482afa61c573fe38630577caff30798838a7bc1
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-08-27 07:22:24 +0100

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 export const BASE_URL =
   process.env.NODE_ENV == "production"
-    ? `http://127.0.0.1:8000/api/v1`
+    ? `https://accommodeme-backend.onrender.com/api/v1`
     : `http://127.0.0.1:8000/api/v1`;
 
 export const AUTH_URL =
   process.env.NODE_ENV == "production"
-    ? `http://127.0.0.1:8000/api/v1`
+    ? `https://accommodeme-backend.onrender.com/api/v1`
     : `http://127.0.0.1:8000/api/v1`;
