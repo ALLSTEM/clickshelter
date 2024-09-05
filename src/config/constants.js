d4fc9e4cd9dce8fff957cd5deb02342baa2e2f95
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-09-05 16:03:56 +0100

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 export const BASE_URL =
   process.env.NODE_ENV == "production"
-    ? `https://api.clickshelter.com/`
+    ? `https://api.clickshelter.com/api/v1`
     : `http://127.0.0.1:8000/api/v1`;
 
 export const AUTH_URL =
   process.env.NODE_ENV == "production"
-    ? `https://api.clickshelter.com/`
+    ? `https://api.clickshelter.com/api/v1`
     : `http://127.0.0.1:8000/api/v1`;
